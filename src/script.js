d6ac399fe21a27c18237bf38bcbc3a7c5088d92e
matheuscosta98-kirby <mathheus.ocosta98@gmail.com>
2025-11-30 16:00:03 -0300

    adicionei nova pagina e varias seções

---
+++ src/script.js
@@ -0,0 +1,16 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const sideMenu = document.querySelector('.side-menu');
+
+menuToggle.addEventListener('click', () => {
+    sideMenu.classList.toggle('active');
+    menuToggle.classList.toggle('active');
+})
+
+//fechar ao clicar nos links
+const menuLinks = document.querySelectorAll('.menu-list a');
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        sideMenu.classList.remove('active');
+        menuToggle.classList.remove('active');
+    });
+})
